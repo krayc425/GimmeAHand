9876aa0e8b120da3d8e42bd3f4d6b4a88c48fcf1
Kuixi Song <krayc425@gmail.com>
2021-03-10 21:14:25 -0800

    add basic storyboards

--- GimmeAHand/AppDelegate.swift
+++ GimmeAHand/AppDelegate.swift
@@ -6,14 +6,17 @@
 //
 
 import UIKit
+import IQKeyboardManagerSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // Setup third-party libraires
+        IQKeyboardManager.shared.enable = true
+        
         return true
     }
 
@@ -31,6 +34,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
-
 }
-
--- README.md
+++ README.md
@@ -5,6 +5,6 @@ Project for Mobile &amp; IoT Computing Services
 # Requirements
 
 1. Install `Cocoapods`: `sudo gem install cocoapods`.
-2. Run `pod install` in root folder.
+2. Run `pod install` in root folder. (If failed, run `rvm use ruby-2.3.0` first).
 3. Open `GimmeAHand.xcworkspace`.
 4. `Cmd+ R` to run.
