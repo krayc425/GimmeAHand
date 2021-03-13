8e8943bc088378337e90c2f97f9fc6a3250f56e9
Kuixi Song <krayc425@gmail.com>
2021-03-12 16:11:09 -0800

    add login logic

---
+++ GimmeAHand/LoginTableViewController.swift
@@ -0,0 +1,55 @@
+//
+//  LoginTableViewController.swift
+//  GimmeAHand
+//
+//  Created by Kuixi Song on 3/12/21.
+//
+
+import UIKit
+import SVProgressHUD
+
+class LoginTableViewController: UITableViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var faceIDSwitch: UISwitch!
+    @IBOutlet weak var loginButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        [emailTextField, passwordTextField].forEach { (textField) in
+            textField?.delegate = self
+        }
+    }
+    
+    @IBAction func loginAction(_ sender: UIButton) {
+        guard sender == loginButton else {
+            debugPrint("Invalid Button!")
+            return
+        }
+        login()
+    }
+    
+    func login() {
+        // TODO: Add login logic
+        debugPrint("Login!!!")
+    }
+
+}
+
+extension LoginTableViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == emailTextField {
+            emailTextField.resignFirstResponder()
+            passwordTextField.becomeFirstResponder()
+        } else if textField == passwordTextField {
+            passwordTextField.resignFirstResponder()
+            login()
+        }
+        return true
+    }
+    
+}
